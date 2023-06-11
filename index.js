feb433e79433f089b9bd1cb36405754e880fd3e5
franciscosucho <110209358+franciscosucho@users.noreply.github.com>
2023-06-11 17:50:39 -0300

    11-6
    est casi terminado,falta pulir los filtros de termos y bombillas

--- index.js
+++ index.js
@@ -1,45 +1,17 @@
 
-var cantidadpixeles = 100;
+var cantidadpixeles = 10;
 
-var filtro_termos = document.querySelector(".filtro_termos");
+var filtro_termos = document.querySelectorAll(".filtro_termos");
 var filtro_imperial = document.querySelector(".filtro_imperial");
-var filtro_bombillas = document.querySelector(".filtro_bombillas");
+var filtro_bombillas = document.querySelectorAll(".filtro_bombillas");
 var filtro_torpedos = document.querySelector(".filtro_torpedos");
 var filtro_todo = document.querySelector(".filtro_todo");
-
-filtro_todo.onclick = () => {
-    let productos = document.querySelectorAll(".articulo");
-    productos.forEach(producto => {
-        producto.style.display = 'flex';
-    });
-}
-
-filtro_torpedos.onclick = () => {
-    filtro('imperial', 'camionero')
-}
-
-filtro_imperial.onclick = () => {
-    filtro('torpedo', 'camionero')
-}
-filtro_camionero.onclick = () => {
-    filtro('torpedo', 'imperial')
-}
-filtro_torpedos.onclick = () => {
-    filtro('imperial', 'camionero')
-}
-
-
-
-function filtro(...clases) {
-    clases.forEach(clase => {
-        const elementos = document.querySelectorAll('.' + clase);
-        elementos.forEach(elemento => {
-            elemento.style.display = 'none';
-        });
-    });
-}
-
+var filtro_termos = document.querySelector(".filtro_termos");
+var filtro_bombillas = document.querySelector(".filtro_bombillas");
+var filtro_mates = document.querySelector(".filtro_mates");
+var inp = document.querySelector(".input");
 window.addEventListener('scroll', function () {
+
     var cont_header = this.document.querySelector(".cont-header");
     var header = document.querySelector(".header");
     var img_logo = document.querySelector(".img-logo");
@@ -51,14 +23,75 @@ window.addEventListener('scroll', function () {
         // Aplica los cambios de estilo al elemento
         cont_header.style.height = '100px';
         modificar_tamaño(img_logo);
-        header.style.height = '200px'
+        header.style.height = '19vh';
     } else {
         cont_header.style.height = 'auto';
-        header.style.height = '300px'
+        header.style.height = '30vh'
         // Restaura los estilos originales del elemento
         restore(img_logo);
     }
 });
+
+
+
+document.addEventListener('keyup', e => {
+    //esto le da un evento a todo el documento cuando se presiona una tecla
+    if (e.target.matches("#buscador")) {
+        document.querySelectorAll('.name').forEach(art => {
+            var contenedorPadre = art.parentNode;
+            art.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+            ?contenedorPadre.classList.remove('filtro')
+            :contenedorPadre.classList.add('filtro')
+        })
+    }
+})
+
+
+
+filtro_termos.addEventListener("click", () => {
+    reset()
+    filtro('imperial', 'camionero', 'bombilla', 'torpedo')
+})
+
+filtro_torpedos.addEventListener("click", () => {
+    reset()
+    filtro('imperial', 'camionero', 'bombilla', 'termo')
+})
+filtro_mates.addEventListener("click", () => {
+    reset()
+    filtro('bombilla', 'termo')
+})
+filtro_todo.addEventListener("click", () => { reset() });
+
+filtro_imperial.addEventListener("click", () => {
+    reset()
+    filtro('torpedo', 'camionero', 'bombilla', 'termo')
+})
+
+filtro_bombillas.addEventListener("click", () => {
+    
+    reset()
+    filtro('imperial', 'camionero', 'torpedo', 'termo')
+})
+
+
+
+
+function filtro(...clases) {
+    clases.forEach(clase => {
+        const elementos = document.querySelectorAll('.' + clase);
+        elementos.forEach(elemento => {
+            elemento.style.display = 'none';
+        });
+    });
+}
+function reset() {
+    let productos = document.querySelectorAll(".articulo");
+    productos.forEach(producto => {
+        producto.style.display = 'flex';
+    });
+}
+
 
 function modificar_tamaño(...elementos) {
 
